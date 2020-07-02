41d13074640765156221c24033b925b320708dca
hayhaydz <haydon1900@gmail.com>
2020-07-02 14:32:19 +0100

    Remove static images

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,20 +2,21 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/static/img`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/img`,
-      },
-    },
+    `gatsby-transformer-remark`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `src`,
+    //     path: `${__dirname}/static/img`,
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `src`,
+    //     path: `${__dirname}/src/img`,
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
