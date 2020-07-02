21918b572190553514221003bd0defe35ba90a39
hayhaydz <haydon1900@gmail.com>
2020-07-02 14:23:30 +0100

    Article

---
+++ src/templates/article.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+const Article = () => {
+    return (
+        <div className="Article">
+            <h1>Hello</h1>
+        </div>
+    )
+}
+export default Article
