a6a8ccff410fe126060380b8164510df57657837
hayhaydz <haydon1900@gmail.com>
2020-07-10 13:15:31 +0100

    CMS Patch

--- src/pages/bts.js
+++ src/pages/bts.js
@@ -1,47 +1,47 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+// import React from 'react'
+// import { graphql } from 'gatsby'
 
-import Layout from '../components/Layout/Layout'
-import ArticleRoll from '../components/ArticleRoll/ArticleRoll'
+// import Layout from '../components/Layout/Layout'
+// import ArticleRoll from '../components/ArticleRoll/ArticleRoll'
 
-const BTS = ({ data }) => {
-    const posts = data.allMarkdownRemark.edges
-    return (
-        <Layout>
-            <ArticleRoll data={posts} />
-        </Layout>
-    )
-}
-export default BTS
+// const BTS = ({ data }) => {
+//     const posts = data.allMarkdownRemark.edges
+//     return (
+//         <Layout>
+//             <ArticleRoll data={posts} />
+//         </Layout>
+//     )
+// }
+// export default BTS
 
-export const pageQuery = graphql`
-query {
-    allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "\/bts/"}}
-        sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "DD/MM/YYYY")
-            author
-            description
-            subtitle
-            title
-            featured_image {
-              childImageSharp {
-                fluid(maxWidth: 500, quality: 100) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+// export const pageQuery = graphql`
+// query {
+//     allMarkdownRemark(
+//         filter: { fileAbsolutePath: { regex: "\/bts/"}}
+//         sort: { fields: [frontmatter___date], order: DESC }
+//     ) {
+//       edges {
+//         node {
+//           fields {
+//             slug
+//           }
+//           frontmatter {
+//             date(formatString: "DD/MM/YYYY")
+//             author
+//             description
+//             subtitle
+//             title
+//             featured_image {
+//               childImageSharp {
+//                 fluid(maxWidth: 500, quality: 100) {
+//                   ...GatsbyImageSharpFluid
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
   
-`
+// `
--- src/pages/fashion.js
+++ src/pages/fashion.js
@@ -1,51 +1,51 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+// import React from 'react'
+// import { graphql } from 'gatsby'
 
-import Layout from '../components/Layout/Layout'
-import TwoPostHero from '../components/TwoPostHero/TwoPostHero'
-import ArticleRoll from '../components/ArticleRoll/ArticleRoll'
+// import Layout from '../components/Layout/Layout'
+// import TwoPostHero from '../components/TwoPostHero/TwoPostHero'
+// import ArticleRoll from '../components/ArticleRoll/ArticleRoll'
 
-const Fashion = ({ data }) => {
-    const posts = data.allMarkdownRemark.edges
-    const firstPosts = posts.splice(0, 2)
+// const Fashion = ({ data }) => {
+//     const posts = data.allMarkdownRemark.edges
+//     const firstPosts = posts.splice(0, 2)
     
-    return (
-        <Layout>
-            <TwoPostHero TwoPosts={firstPosts}/>
-            <ArticleRoll data={posts} />
-        </Layout>
-    )
-}
-export default Fashion
+//     return (
+//         <Layout>
+//             <TwoPostHero TwoPosts={firstPosts}/>
+//             <ArticleRoll data={posts} />
+//         </Layout>
+//     )
+// }
+// export default Fashion
 
-export const pageQuery = graphql`
-query {
-    allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "\/fashion/"}}
-        sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "DD/MM/YYYY")
-            author
-            description
-            subtitle
-            title
-            featured_image {
-              childImageSharp {
-                fluid(maxWidth: 1920, quality: 100) {
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+// export const pageQuery = graphql`
+// query {
+//     allMarkdownRemark(
+//         filter: { fileAbsolutePath: { regex: "\/fashion/"}}
+//         sort: { fields: [frontmatter___date], order: DESC }
+//     ) {
+//       edges {
+//         node {
+//           fields {
+//             slug
+//           }
+//           frontmatter {
+//             date(formatString: "DD/MM/YYYY")
+//             author
+//             description
+//             subtitle
+//             title
+//             featured_image {
+//               childImageSharp {
+//                 fluid(maxWidth: 1920, quality: 100) {
+//                   ...GatsbyImageSharpFluid_withWebp
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
   
-`
+// `
