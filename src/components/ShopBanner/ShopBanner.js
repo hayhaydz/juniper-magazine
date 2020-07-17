fe02962b5cffac77c85a4827e20d10d7c083e90a
hayhaydz <haydon1900@gmail.com>
2020-07-17 12:30:34 +0100

    Shop Cover lazy loading

--- src/components/ShopBanner/ShopBanner.js
+++ src/components/ShopBanner/ShopBanner.js
@@ -1,15 +1,35 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 
 const ShopBanner = () => {
     return (
-        <div className="ShopBanner">
-            <Link to={`/shop`} className="ShopBanner__link">
-                <div className="ShopBanner__link--banner">
-                    <h1 className="ShopBanner__link--heading">Issue No.1 // Juniper's First <br/> View Here</h1>
+        <StaticQuery 
+            query={graphql`
+                query {
+                    file(relativePath: { eq: "img/shopCover.jpg" }) {
+                        childImageSharp {
+                            fluid(maxWidth: 1920, quality: 100) {
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                        }
+                    }
+                }
+            `}
+            render={data => (
+                <div className="ShopBanner">
+                    <Link to={`/shop`} className="ShopBanner__link">
+                        <BackgroundImage
+                            className="ShopBanner__link--banner"
+                            fluid={data.file.childImageSharp.fluid}
+                        >
+                            <h1 className="ShopBanner__link--heading">Issue No.1 // Juniper's First <br/> View Here</h1>
+                            <div className="ShopBanner__link--tint"></div>
+                        </BackgroundImage>
+                    </Link>
                 </div>
-            </Link>
-        </div>
+            )}
+        />
     )
 }
 export default ShopBanner
